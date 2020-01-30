14da2a3d4fd3de54108ac6639d64f4dc1f9e31d6
belu21 <belupoga@gmail.com>
2020-01-30 10:56:21 -0400

    navdropdown

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import '../css/estiloNavbar.css'
 
 class NavBar extends React.Component {
     render() {
-        return <div><Navbar bg="dark" expand="lg" variant="dark" fixed="top">
+        return <div><Navbar bg="dark" expand="lg" variant="dark" fixed="top" className='p-3'>
             <Link to="/" className="navbar-brand">CommIT SRL</Link>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
                     <Link to="/" className="nav-link">Home</Link>
-                    <Link to="/nosotros" className="nav-link">Nosotros</Link>
+        
+                    <NavDropdown title="Nosotros" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Another action</NavDropdown.Item>
+                        <NavDropdown.Item href="#">Something</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    
                     <Link to="/servicios" className="nav-link">Servicios y Soluciones</Link>
                     <Link to="/equipamiento" className="nav-link">Equipamiento</Link>
                     <Link to="/partners" className="nav-link">Partners</Link>
@@ -19,7 +28,7 @@ class NavBar extends React.Component {
                     <Link to="/casosexito" className="nav-link">Casos de Exito</Link>
                 </Nav>
             </Navbar.Collapse>
-        </Navbar><br/><br/></div>
+        </Navbar><br /><br /></div>
     }
 }
 export default NavBar;
