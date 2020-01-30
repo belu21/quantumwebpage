a413474a017c46017efbe504d1180d50a1a1dabf
skynoorz <aligatorbol@gmail.com>
2020-01-30 12:09:29 -0500

    servicios anadido

--- src/components/Servicios.js
+++ src/components/Servicios.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import servicio from '../json/servicio'
+import Tarjeta from './Tarjeta'
 
-function Servicios (){
-    return <div>
-       
-        <h1>Servicios PRUEBA</h1>
-    </div>;
+class Servicios extends React.Component {
+
+    state = {
+        servicio: servicio
+    }
+    render() {
+        return <div className="py-4 mw-100 w-100">
+            <Tarjeta info={this.state.servicio.softEmpresarial} />
+            <Tarjeta info={this.state.servicio.openSource} />
+            <Tarjeta info={this.state.servicio.javajboss} />
+            <Tarjeta info={this.state.servicio.asistenciaTec} />
+            <Tarjeta info={this.state.servicio.capacitacion} />
+            {/* <Tarjeta info={this.state.servicio.consultoria} /> */}
+            <Tarjeta info={this.state.servicio.javajboss} />
+            <Tarjeta info={this.state.servicio.bussiness} />
+            <Tarjeta info={this.state.servicio.procesosNegocio} />
+        </div>;
+    }
 }
 
 export default Servicios;
