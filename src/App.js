8a50878dc7673132d1c16e306be9d6448bfc65e3
skynoorz <aligatorbol@gmail.com>
2020-02-14 16:30:44 -0500

    tarjetas mejoradas y sin errores

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Contactanos from './components/Contactanos'
 import Tarjeta from "./components/Tarjeta";
 
 import nosotros from './json/nosotros'
-import servicio from './json/servicio'
 
 function App() {
 
@@ -37,45 +36,47 @@ function App() {
 
 }
 
-const Home = () => (<div>
-    <div className="tarjeta-home">
-        <div className="cabecera">
-            <div className="titulo-home">
-                CommIT
-            </div>
-            <div className="tarjeta-subhome container px-6">
-                <p>Una empresa enmarcada en el sector de las nuevas Tecnologías de la Información y Comunicación (TICs)
-                    ofrece servicios especializados de consultoría, integrando soluciones de alta calidad tanto en las
-                    administraciones públicas como en el sector privado.</p>
-            </div>
-            <div className="tarjeta-subhome1 container px-6">
-                <p>
-                    La empresa CommIT tiene el nombre emblemático que representa la Tecnología en todo ámbito.
-                    Actualmente contamos con un grupo de programadores y
-                    expertos que la conforman y que ya llevan muchos años trabajando en dar soluciones IT a clientes
-                    particulares y empresas.
-                </p>
+const Home = () => {
+    return (<div>
+            <div className="tarjeta-home">
+                <div className="cabecera">
+                    <div className="titulo-home">
+                        CommIT
+                    </div>
+                    <div className="tarjeta-subhome container px-6">
+                        <p>Una empresa enmarcada en el sector de las nuevas Tecnologías de la Información y Comunicación
+                            (TICs)
+                            ofrece servicios especializados de consultoría, integrando soluciones de alta calidad tanto
+                            en las
+                            administraciones públicas como en el sector privado.</p>
+                    </div>
+                    <div className="tarjeta-subhome1 container px-6">
+                        <p>
+                            La empresa CommIT tiene el nombre emblemático que representa la Tecnología en todo ámbito.
+                            Actualmente contamos con un grupo de programadores y
+                            expertos que la conforman y que ya llevan muchos años trabajando en dar soluciones IT a
+                            clientes
+                            particulares y empresas.
+                        </p>
 
+                    </div>
+                </div>
+                <div className="headivs">
+                    <p>ACERCA DE COMMIT</p>
+                    <div>
+                        <h5>Somos una empresa especializada en ofrecer servicios de vanguardia a los
+                            clientes</h5>
+                    </div>
+
+                </div>
+            </div>
+            <div className="footer-tarjeta">
+                <Tarjeta info={nosotros.mision}/>
+                <Tarjeta info={nosotros.vision}/>
             </div>
         </div>
-        <div className="headivs">
-            <p className="h5">ACERCA DE COMMIT</p>
-            <div className="jumbotron">
-                <h5 className="headivs2">Somos una empresa especializada en ofrecer servicios de vanguardia a los
-                    clientes</h5>
-            </div>
-
-        </div>
-
-
-    </div>
-        <div className="footer-tarjeta">
-            <Tarjeta info={nosotros.mision}></Tarjeta>
-            <Tarjeta info={servicio.desarrolloSoftware}></Tarjeta>
-            
-        </div>
-    </div>
-)
+    );
+}
 
 
 export default App;
--- src/components/Tarjeta.js
+++ src/components/Tarjeta.js
@@ -2,41 +2,15 @@ import React from 'react'
 import '../css/estiloTarjeta.css'
 
 class Tarjeta extends React.Component {
+
     render() {
-        // var json = this.props.info
-        // if (this.props.info.subs == null) {
-        //     return <div className="py-4 mw-100 w-100 container">
-        //         <div className="card text-white bg-dark mb-3">
-        //             <div className="card-header bg-danger">{this.props.info.titulo}</div>
-        //             <div className="card-body-custom card-body">
-        //                 <p className="card-text">{this.props.info.texto}</p>
-        //             </div>
-        //         </div>
-        //     </div>;
-        // } else {
-        //     return <div className="py-4 mw-100 w-100 container">
-        //         <div className="card text-white bg-dark mb-3">
-        //             <div className="card-header bg-danger">{this.props.info.titulo}</div>
-        //             <div className="card-body-custom card-body">
-        //                 <p className="card-text">{this.props.info.texto}</p>
-        //                 {/* <SubTarjeta info={json}></SubTarjeta> */}
-        //                 {
-        //                     Object.values(json.subs).map((key) => {
-        //                         return (
-        //                             <li>{key}</li>
-        //                         )
-        //                     })
-        //                 }
-        //             </div>
-        //         </div>
-        //     </div>;
-        // }
-        // ;
-        return <div className="card tarjetita">
-            {/*<h4>hola</h4>*/}
-            <div className="title">{this.props.info.titulo}</div>
-            <p>{this.props.info.texto}</p>
-            <a href="#">Leer más</a>
+        return <div className="tarjetita">
+            <div className="card">
+                {/*<h4>hola</h4>*/}
+                <div className="title">{this.props.info.titulo}</div>
+                <p>{this.props.info.texto}</p>
+                <a href="/">Leer más</a>
+            </div>
         </div>
     }
 }
